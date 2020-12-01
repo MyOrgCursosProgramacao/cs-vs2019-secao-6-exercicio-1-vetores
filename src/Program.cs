7b02de4a38d998cefe3945a95fd2855b2c8c830c
Diego <alvarodiego@gmail.com>
2020-12-01 19:26:50 -0300

    head do programa

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,13 @@ namespace src
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Resolução do Exercício proposto 1 da Seção 6" + Environment.NewLine);
+            Console.WriteLine("Entre com os dados dos alugueis dos quartos.");
+
+            Console.Write(Environment.NewLine + "Quantos alugueis serão registrados? ");
+            int n = int.Parse(Console.ReadLine);
+            AluguelQuarto[] aluguelQuarto = new AluguelQuarto[10];
+
         }
     }
 }
